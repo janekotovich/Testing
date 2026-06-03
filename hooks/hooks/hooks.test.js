57828f4393ad3261e543405323ab7c1cb2d1cae9
Jane Kotovich <jenyakotovich@gmail.com>
2026-06-03 15:33:34 +1000

    Concurrent usage for parallel execution

--- hooks/hooks/hooks.test.js
+++ hooks/hooks/hooks.test.js
@@ -27,14 +27,14 @@ describe("useCreateUser()", () => {
     console.log("afterAll");
   });
 
-  it("should update the email", () => {
+  it.concurrent("should update the email", () => {
     const newTestEmail = "test2@test.com";
     user.updateEmail(newTestEmail);
 
     expect(user.email).toBe(newTestEmail);
   });
 
-  it("should have an email property", () => {
+  it.concurrent("should have an email property", () => {
     expect(user).toHaveProperty("email");
   });
 
