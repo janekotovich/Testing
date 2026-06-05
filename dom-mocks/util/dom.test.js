23af149b6031e0fc2df59db876263563c89e209e
Jane Kotovich <jenyakotovich@gmail.com>
2026-06-05 19:42:34 +1000

    Init happy dom doc to write html and work with it

--- dom-mocks/util/dom.test.js
+++ dom-mocks/util/dom.test.js
@@ -1,5 +1,15 @@
 import { describe, it, expect } from "vitest";
 import { showError } from "./dom.js";
+import fs from "fs";
+import path from "path";
+import { Window } from "happy-dom";
+
+const htmlDocPath = path.join(process.cwd(), "index.html");
+const htmlDocContent = fs.readFileSync(htmlDocPath).toString();
+
+const window = new Window();
+const document = window.document;
+document.write(htmlDocContent);
 
 describe("showError()", () => {
   it("Shows err", () => {
